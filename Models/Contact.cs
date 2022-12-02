b9a6da1dba7f69ac2a8f1f7b94b0decfe612cd69
Linto <lintov7@gmail.com>
2022-12-02 16:31:08 -0500

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+
+        public int ContactId { get; set; }
+        [Required]  
+        public string? Name { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string? Email { get; set; }
+
+
+        // user ID from AspNetUser table.
+        public string? OwnerID { get; set; }
+
+        public ContactStatus Status { get; set; }
+    }
+
+    public enum ContactStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
+    }
+}
